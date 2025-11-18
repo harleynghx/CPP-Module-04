d07ca09454c9e07f009f59915b6a227546e3cf9f
harleynghx <harleynghx.edu@gmail.com>
2025-11-19 05:26:16 +0800

    update brain: :

--- ex01/srcs/dog.cpp
+++ ex01/srcs/dog.cpp
@@ -2,25 +2,39 @@
 
 Dog::Dog() {
     _type = "Dog";
+    brain = new Brain();
     std::cout << "Dog default constructor called" << std::endl;
 }
 
 Dog::Dog(const Dog& other) : Animal(other) {
     std::cout << "Dog copy constructor called" << std::endl;
+    brain = new Brain(*other.brain);
 }
 
 Dog& Dog::operator=(const Dog& other) {
     std::cout << "Dog copy assignment operator called" << std::endl;
     if (this != &other) {
         Animal::operator=(other);
+        delete brain; //? delete old brain
+        brain = new Brain(*other.brain);
     }
     return *this;
 }
 
 Dog::~Dog() {
+    delete brain;
     std::cout << "Dog destructor called" << std::endl;
 }
 
 void Dog::makeSound() const {
     std::cout << "what da dog doin" << std::endl;
+}
+
+Brain* Dog::getBrain() const {
+    return brain;
+}
+
+void Dog::setIdea(unsigned int index, const std::string& idea) {
+    if (brain) 
+        brain->setIdea(index, idea);
 }
